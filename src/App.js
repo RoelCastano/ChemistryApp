0af15206448718cfdd4f0e291b336909845d60d6
Beto Esquivel <ppbeto94@gmail.com>
2017-04-19 20:56:21 -0500

    Installs dependencies and sets up bulma with sass compilation.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,39 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="section">
+        Sup, dude... This works with bulma.
+        <div className="container box">
+          <article className="media">
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img src="http://bulma.io/images/placeholders/128x128.png" alt="Image"/>
+              </figure>
+            </div>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                  <br/>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                </p>
+              </div>
+              <nav className="level is-mobile">
+                <div className="level-left">
+                  <a className="level-item">
+                    <span className="icon is-small"><i className="fa fa-reply"></i></span>
+                  </a>
+                  <a className="level-item">
+                    <span className="icon is-small"><i className="fa fa-retweet"></i></span>
+                  </a>
+                  <a className="level-item">
+                    <span className="icon is-small"><i className="fa fa-heart"></i></span>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </article>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
