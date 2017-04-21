5c1c04eb53f8db29e1bbd07cf3c838781dfada58
Beto Esquivel <ppbeto94@gmail.com>
2017-04-20 21:55:07 -0500

    Basic navigation with navbar

--- src/TopNavbar.js
+++ src/TopNavbar.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import './TopNavbar.css';
 
 class TopNavbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggled: false,
+    };
+  }
+
+  toggleMenu = () =>
+    this.setState({
+      toggled: !this.state.toggled,
+    });
+
   render() {
+    const isActive = this.state.toggled ?
+      'is-active' : '';
     return (
       <nav className="nav">
         <div className="nav-left">
-          <a className="nav-item">
+          <NavLink to={'/'}
+            className="nav-item"
+            activeClassName="is-active"
+            >
             <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo" />
-          </a>
+          </NavLink>
         </div>
 
         <div className="nav-center">
@@ -24,22 +42,27 @@ class TopNavbar extends Component {
           </a>
         </div>
 
-        <span className="nav-toggle">
+        <span
+          onClick={this.toggleMenu}
+          className={`nav-toggle ${isActive}`}>
           <span></span>
           <span></span>
           <span></span>
         </span>
 
-        <div className="nav-right nav-menu">
-          <a className="nav-item">
-            Home
-          </a>
-          <a className="nav-item">
-            Documentation
-          </a>
-          <a className="nav-item">
-            Blog
-          </a>
+        <div className={`nav-right nav-menu ${isActive}`}>
+          <NavLink to={'/reaction'}
+            className="nav-item is-tab"
+            activeClassName="is-active"
+            >
+            Reaction
+          </NavLink>
+          <NavLink to={'/exam'}
+            className="nav-item is-tab"
+            activeClassName="is-active"
+            >
+            Exam
+          </NavLink>
 
           <div className="nav-item">
             <div className="field is-grouped">
