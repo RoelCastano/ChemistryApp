143aca0c8b77a3f6de751ad329e8054dbd279f08
Beto Esquivel <ppbeto94@gmail.com>
2017-04-20 20:54:09 -0500

    Adds nav bar skeleton and fontawesome.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+
+import TopNavbar from './TopNavbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="section">
-        Sup, dude... This works with bulma.
-        <div className="container box">
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-64x64">
-                <img src="http://bulma.io/images/placeholders/128x128.png" alt="Image"/>
-              </figure>
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-                  <br/>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                </p>
-              </div>
-              <nav className="level is-mobile">
-                <div className="level-left">
-                  <a className="level-item">
-                    <span className="icon is-small"><i className="fa fa-reply"></i></span>
-                  </a>
-                  <a className="level-item">
-                    <span className="icon is-small"><i className="fa fa-retweet"></i></span>
-                  </a>
-                  <a className="level-item">
-                    <span className="icon is-small"><i className="fa fa-heart"></i></span>
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </article>
+      <div>
+        <TopNavbar />
+        <div className="section">
         </div>
       </div>
     );
