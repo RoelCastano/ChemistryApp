b25eddb72ad4326d0ce91dc58a2fd7ae58e6ca3e
Beto Esquivel <ppbeto94@gmail.com>
2017-04-20 21:55:07 -0500

    Basic navigation with navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 
 import TopNavbar from './TopNavbar';
+import Home from './Home';
+import Reaction from './Reaction';
+import Exam from './Exam';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <TopNavbar />
-        <div className="section">
+      <Router>
+        <div id="main-content">
+          <TopNavbar />
+          <Switch>
+            <Route path="/reaction" component={Reaction}>
+            </Route>
+            <Route path="/exam" component={Exam}>
+            </Route>
+            <Route path="/" component={Home}></Route>
+          </Switch>
         </div>
-      </div>
+      </Router>
     );
   }
 }
