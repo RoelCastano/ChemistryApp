331f2b8db2d3e09458fd40deef43452cb07167e6
Melissa Treviño <mely.trevic@gmail.com>
2017-04-23 01:00:13 -0500

    Finished details of Home component

--- src/Home.js
+++ src/Home.js
@@ -6,15 +6,20 @@ class Home extends Component {
   render() {
     return (
       <div className="section">
-        <div className="front">
-          <div className="container is-fluid">
-            <p className="title">
-                Reacción de obtención de analgésicos
-            </p>
-            <p className="subtitle">
-                Esta unidad estudia los fundamentos de las reacciones para 
-                la obtención de fármacos analgésicos.
-            </p>
+        <div className="displayBox">
+          <div className="content">
+            <div className="container is-fluid">
+              <p className="title">
+                  Reacción de obtención de analgésicos
+              </p>
+              <p className="subtitle">
+                  Esta unidad estudia los fundamentos de las reacciones para 
+                  la obtención de fármacos analgésicos.
+              </p>
+            </div>
+          </div>
+          <div className="buttons">
+            <a className="button continue">COMENZAR</a>
           </div>
         </div>
       </div>
