0b76d153f9cd7460d44604b19e97caf535909b9f
Melissa Treviño <mely.trevic@gmail.com>
2017-04-23 00:06:46 -0500

    Added content and style on Home component

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './Home.css';
+import image from './Image.png';
 
 class Home extends Component {
   render() {
     return (
       <div className="section">
-        <p>
-          Hello from Home.
-        </p>
+        <div className="front">
+          <div className="container is-fluid">
+            <p className="title">
+                Reacción de obtención de analgésicos
+            </p>
+            <p className="subtitle">
+                Esta unidad estudia los fundamentos de las reacciones para 
+                la obtención de fármacos analgésicos.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
