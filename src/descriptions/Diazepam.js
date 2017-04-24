2b93171a940de0eb8857e4f53d827fd203dea9e2
Melissa Treviño <mely.trevic@gmail.com>
2017-04-24 02:10:07 -0500

    To each description, added alternative name and complete reaction image

--- src/descriptions/Diazepam.js
+++ src/descriptions/Diazepam.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './Desc.css';
+import image from '../reactions/Valium/ValiumComplete.png';
 
 class Diazepam extends Component {
   render() {
     return (
       <div className="content">
-      <h2>DIAZEPAM</h2>
+      <h2>Valium</h2>
+      <h4>(Diazepam)</h4>
+      <img src={image}/>
         <p>
-En esta reacción de sustitución electrofílica el ácido de lewis AlCl3 toma el cloro generando un carbonation donde el reactivo se unirá. El medio también donará un protón para convertir al doble enlace carbono oxígeno en un alcohol, Dando como resultado la molécula final.
+          En esta reacción de sustitución electrofílica el ácido de lewis AlCl3 
+          toma el cloro generando un carbonation donde el reactivo se unirá. 
+          El medio también donará un protón para convertir al doble enlace 
+          carbono oxígeno en un alcohol, Dando como resultado la molécula final.
         </p>
 
       </div>
--- src/descriptions/Librium.js
+++ src/descriptions/Librium.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './Desc.css';
+import image from '../reactions/Librium/LibriumComplete.png';
 
 class Librium extends Component {
   render() {
     return (
       <div className="content">
-      <h2>LIBRIUM</h2>
+      <h2>Librium</h2>
+      <h4>(Clordiazepoxido)</h4>
+      <img src={image}/>
         <p>
 La hidroxilamina expulsa al oxígeno del carbonilla en forma de agua para tomar su lugar en la molécula principal (sustitución nucleofílica). Posteriormente el Cl2O debido a al tamaño y geometría de la molécula cierra el ciclo enlazándose con los dos nitrógenos.
         </p>
