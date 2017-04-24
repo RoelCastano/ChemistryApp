6b888c01cc035a4f4b55605277a0c56b1f7e8385
Roel Castaño <roelcastanomoreno@gmail.com>
2017-04-23 20:38:16 -0500

    Adds descriptions to Reactions

---
+++ src/descriptions/Ibuprofeno.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Desc.css';
+
+class Ibuprofeno extends Component {
+  render() {
+    return (
+      <div className="descriptions">
+      <h2>IBUPROFENO</h2>
+        <p>
+        <ol>
+<li>En los pasos de estas reacciones puedes observar una sustitución electrofílica en la primera etapa.</li>
+<li>En el segundo paso la carga parcial negativa del etil-2-cloroacetato forman un enlace con el grupo  carbonilo.</li>
+<li>Tercer paso se hidrata la molécula para deshacer el enlace epóxido y formar un ester gracias a la atracción de cargas generadas entre el H+ (ácido) y los oxígeno del enlace. al final se obtiene etanol como un subproducto.</li>
+<li>Cuarto paso la resonancia de la molécula genera una carga positiva en un carbono del éster lo cual atrae al nucleófilo (hidroxilamina) sustituyendo así este último compuesto al carbonilo original.</li>
+<li>En esta etapa el medio ácido dona un protón al OH unido al nitrógeno, volviéndolo en una molécula de agua ideal para ser un grupo saliente. Finalmente esto crea una triple enlace Carbono Nitrógeno conocido como nitrilo.</li>
+<li>El agua opera como un ácido en este paso haciendo una reacción ácido base con el nitrógeno del nitrilo y sustituyendo dentro del compuesto principal en forma de Ácido Carboxílico</li>
+</ol>
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default Ibuprofeno;
---
+++ src/descriptions/Tylenol.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Desc.css';
+
+class Tylenol extends Component {
+  render() {
+    return (
+      <div className="descriptions">
+      <h2>TYLENOL</h2>
+        <p>
+Esta es una sustitución nucleofílica acílica, conde la amiga y el anillo aromático actual como el agente alicante uniéndose al grupo Ac. para formar la molécula del paracetamol.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default Tylenol;
