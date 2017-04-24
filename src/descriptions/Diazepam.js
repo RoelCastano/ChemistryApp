14af6746e4f46f1fda1b21edf5d67d639a3353ef
Roel Castaño <roelcastanomoreno@gmail.com>
2017-04-23 20:38:16 -0500

    Adds descriptions to Reactions

---
+++ src/descriptions/Diazepam.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Desc.css';
+
+class Diazepam extends Component {
+  render() {
+    return (
+      <div className="descriptions">
+      <h2>DIAZEPAM</h2>
+        <p>
+En esta reacción de sustitución electrofílica el ácido de lewis AlCl3 toma el cloro generando un carbonation donde el reactivo se unirá. El medio también donará un protón para convertir al doble enlace carbono oxígeno en un alcohol, Dando como resultado la molécula final.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default Diazepam;
