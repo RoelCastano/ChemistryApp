bb8d8f7dc2e3ec583e8805a4f18fd2e61d3ec951
Beto Esquivel <ppbeto94@gmail.com>
2017-04-23 23:27:05 -0500

    Makes component that encompasses a react drag and drop question.

--- src/ReactionPractice.js
+++ src/ReactionPractice.js
@@ -107,6 +107,64 @@ const DraggableAnswer = DragSource(
   })
 )(DragAnswer);
 
+class DragDropReactionQuestion extends Component {
+  render () {
+    const {
+      //instructions,
+      reactionId,
+      //question,
+      reaction,
+      options,
+    } = this.props;
+    const question = '¿Qué debe de ir en el espacio?';
+    const instructions = 'Usa tu cursor para arrastrar la opción correcta al recuadro.'
+    return (
+      <div>
+        <div
+          className="question columns">
+          <div className="column has-text-centered">
+            <p className="title is-3">
+              {question}
+            </p>
+            <p className="subtitle is-4">
+              {instructions}
+            </p>
+          </div>
+        </div>
+        <div
+          className="problem columns">
+          <div className="column is-one-third">
+            <QuestionDroppableBin />
+          </div>
+          <div className="column is-two-thirds">
+            <figure className="image">
+              <img
+                alt={`Reaction for ${reactionId} without result`}
+                src={reaction} />
+            </figure>
+          </div>
+        </div>
+        <div
+          className="options columns is-multiline">
+          {options.map((option, i) =>
+            <div key={i} className="column is-half">
+              <DraggableAnswer index={i} option={option} />
+            </div>
+          )}
+        </div>
+        <div className="controls columns">
+          <div
+            className="column is-half is-offset-one-quarter">
+            <a
+              className="button is-fullwidth is-large is-primary">
+              is-one-quarter
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class ReactionPractice extends Component {
   render() {
@@ -118,47 +176,13 @@ class ReactionPractice extends Component {
       <div className="section">
         <div className="card">
           <div className="card-content">
-            <div
-              className="question columns">
-              <div className="column has-text-centered">
-                <p className="title is-3">
-                  ¿Qué debe de ir en el espacio?
-                </p>
-                <p className="subtitle is-4">
-                  Usa tu cursor para arrastrar la opción correcta al recuadro.
-                </p>
-              </div>
-            </div>
-            <div
-              className="problem columns">
-              <div className="column is-one-third">
-                <QuestionDroppableBin />
-              </div>
-              <div className="column is-two-thirds">
-                <figure className="image">
-                  <img
-                    alt={`Reaction for ${reactionId} without result`}
-                    src={reaction} />
-                </figure>
-              </div>
-            </div>
-            <div
-              className="options columns is-multiline">
-              {options.map((option, i) =>
-                <div key={i} className="column is-half">
-                  <DraggableAnswer index={i} option={option} />
-                </div>
-              )}
-            </div>
-            <div className="controls columns">
-              <div
-                className="column is-half is-offset-one-quarter">
-                <a
-                  className="button is-fullwidth is-large is-primary">
-                  is-one-quarter
-                </a>
-              </div>
-            </div>
+          <DragDropReactionQuestion
+            {...{
+              reactionId,
+              reaction,
+              options,
+            }}
+            />
           </div>
         </div>
       </div>
